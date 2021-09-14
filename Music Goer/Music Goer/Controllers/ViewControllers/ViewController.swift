496b4046d079b259bf3a6285f53a7d27f4871f99
Ben Erekson <Ben Erekson>
2021-09-14 13:43:53 -0600

    Add Xcode project

---
+++ Music Goer/Music Goer/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Goer
+//
+//  Created by Ben Erekson on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
