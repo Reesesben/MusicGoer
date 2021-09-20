bda6ae30b9cd6de1472958302423b766d1846f7f
Kyle Warren <basedinbham@gmail.com>
2021-09-20 15:18:31 -0700

    google sign in added

--- Music Goer/Music Goer/Controllers/ViewControllers/Login Storyboard/LoginViewController.swift
+++ Music Goer/Music Goer/Controllers/ViewControllers/Login Storyboard/LoginViewController.swift
@@ -1,28 +1,66 @@
 //
-//  LoginViewController.swift
+//  FirstLoginViewController.swift
 //  Music Goer
 //
-//  Created by Ben Erekson on 9/15/21.
+//  Created by Ben Erekson on 9/20/21.
 //
 
 import UIKit
+import GoogleSignIn
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    //MARK: - IBOutlets
+    @IBOutlet var guestButton: UIButton!
+    @IBOutlet weak var googleSignIn: GIDSignInButton!
+    
+    
+    //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        guestButton.layer.cornerRadius = guestButton.frame.height / 2
+    }
+    
+    
+    
+    //MARK: - ACTIONS
+    @IBAction func googleSignInButtonTapped(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
 
-        // Do any additional setup after loading the view.
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+          if let error = error {
+            // ...
+            return
+          }
+
+          guard
+            let authentication = user?.authentication,
+            let idToken = authentication.idToken
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: authentication.accessToken)
+
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                  return
+                }
+                //performSegue(withIdentifier: "toDetailVC", sender: self)
+            }
+        }
     }
     
-    @IBAction func cancelButtonTapped(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
+    func isAuth(
     
-    @IBAction func LoginButtonTapped(_ sender: Any) {
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyBoard.instantiateViewController(withIdentifier: "MainTabBarController")
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true, completion: nil)
-    }
-}
+    
+} // End of Class
+
+
