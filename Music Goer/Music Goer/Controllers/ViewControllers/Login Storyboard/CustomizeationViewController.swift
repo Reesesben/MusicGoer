cdc0e4f1044e7cb2fb122322245eb55cb8ac6bbe
Ben Erekson <42879076+Reeseben@users.noreply.github.com>
2021-09-30 12:35:32 -0600

    Fix Bugs

--- Music Goer/Music Goer/Controllers/ViewControllers/Login Storyboard/CustomizeationViewController.swift
+++ Music Goer/Music Goer/Controllers/ViewControllers/Login Storyboard/CustomizeationViewController.swift
@@ -12,10 +12,12 @@ class CustomizeationViewController: UIViewController {
     //MARK: - IBOutlets
     @IBOutlet var finishButton: UIButton!
     @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var loadingWheel: UIActivityIndicatorView!
     
     //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadingWheel.isHidden = true
         colorGradient()
         finishButton.layer.cornerRadius = finishButton.frame.height / 2
         self.hideKeyboardWhenTappedAround()
@@ -23,6 +25,13 @@ class CustomizeationViewController: UIViewController {
     }
     //MARK: - Properties
     var userImage: UIImage?
+    var isLoading: Bool = false {
+        didSet {
+            loadingWheel.isHidden = !isLoading
+            isLoading ? loadingWheel.startAnimating() : loadingWheel.stopAnimating()
+            finishButton.isEnabled = !isLoading
+        }
+    }
     
     //MARK: - Helper Func
     func colorGradient() {
@@ -38,18 +47,25 @@ class CustomizeationViewController: UIViewController {
     
     //MARK: - Actions
     @IBAction func finishButtonTapped(_ sender: Any) {
+        isLoading = true
         if userImage == nil {
             displayError(title: "You need to select an image", Body: "You need to select an image so other poeple can find you on the app.")
+            isLoading = false
         } else if (usernameTextField.text == "") {
             displayError(title: "You need to provide a username", Body: "You need to provide a username so users can identify you in the app")
+            isLoading = false
         } else {
             guard let userImage = userImage,
                   let username = usernameTextField.text, !username.isEmpty,
                   let user = Auth.auth().currentUser,
-                  let imageData = userImage.jpegData(compressionQuality: 0.5) else { return }
+                  let imageData = userImage.jpegData(compressionQuality: 0.5) else {
+                      isLoading = false
+                      return
+                  }
             
             MUserController.shared.createUser(userName: username, userImage: imageData, googleRef: user.uid) { sucess in
                 if sucess {
+                    self.isLoading = false
                 self.displayError(title: "Community Guidlines", Body: "If 30 or more users report your account for spam or inapropriate conduct or harasment your account will be blocked for 30 days.") { _ in
                     DispatchQueue.main.async {
                         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -59,6 +75,7 @@ class CustomizeationViewController: UIViewController {
                     }
                 }
                 } else {
+                    self.isLoading = false
                     self.displayError(title: "An Error occurred", Body: "An error occurred creating your account, please ensure you are connected to internet.", completion: nil)
                 }
             }
