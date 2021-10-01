cc2006c77ef6fe847f73e18896ee3cac586feeb5
Kyle Warren <basedinbham@gmail.com>
2021-10-01 10:20:36 -0700

    storyboard bug fixed

--- Music Goer/Music Goer/Resources/AppDelegate.swift
+++ Music Goer/Music Goer/Resources/AppDelegate.swift
@@ -14,8 +14,6 @@ import GoogleSignIn
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -48,8 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
             return GIDSignIn.sharedInstance.handle(url)
         }
         
-        
-        
         // MARK: UISceneSession Lifecycle
         
         func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -63,7 +59,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
             // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
             // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
         }
-        
-        
     }
 } // End of Class
